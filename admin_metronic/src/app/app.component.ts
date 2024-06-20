1fdccd10992b9e3913367b92b960d4dd82476d1c
Carlos Aldana <carlos.aldana@gmail.com>
2024-06-19 23:23:09 -0500

    changes

--- admin_metronic/src/app/app.component.ts
+++ admin_metronic/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashScreenModule } from './_metronic/partials/layout/splash-screen/splash-screen.module';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SplashScreenModule]
 })
 export class AppComponent {
   title = 'admin_metronic';
