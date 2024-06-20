485a9d6fe496db1327044a5ff6dd029a37d5acba
Carlos Aldana <carlos.aldana@gmail.com>
2024-06-19 20:29:43 -0500

    add adminWeb project

--- webClient/src/app/app.component.ts
+++ webClient/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { UserComponent } from "./user/user.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, UserComponent]
+    imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'Proyect Principal';
