9100b9442889e7c82f727f4e388d1b0140e46101
Carlos Aldana <carlos.aldana@gmail.com>
2024-06-28 17:43:39 -0500

    changes

--- webClient/src/app/_services/auth.service.ts
+++ webClient/src/app/_services/auth.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
 import { catchError, map } from 'rxjs/operators';
